299f8c48d2c5ecd8774cbe716bb5a099529a3e42
Lary Renderos <owner@Owners-MacBook-Air.local>
2023-12-14 13:54:51 -0500

    added things

--- playlist.js
+++ playlist.js
@@ -4,6 +4,12 @@ function init() {
     var button = document.getElementById("addButton");
     button.onclick = handleButtonClick;
     loadPlaylist();
+    var loc = window.location.href;
+    var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+    var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
+        loc + '?profile=css3';
+    document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
+    document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
 }
 function handleButtonClick() { 
     var textInput = document.getElementById("songTextInput"); 
@@ -31,12 +37,3 @@ function handleButtonClick() {
           li.removeChild(listItem);
 })*/
 }
-function init() {
-    var loc = window.location.href;
-    var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
-    var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
-                            loc + '?profile=css3';
-    document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
-    document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
-    }
-    window.onload = init;
